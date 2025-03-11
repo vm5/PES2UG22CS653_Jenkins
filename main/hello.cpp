72ac4718ecc9a0b88ecd89a880602653b1ee6406
vedant PES2UG22CS653 <62893030+vm5@users.noreply.github.com>
2025-03-11 21:58:06 +0530

    Update hello.cpp

--- main/hello.cpp
+++ main/hello.cpp
@@ -6,6 +6,6 @@ int main(void) {
   cout << "Hello, World\n";
   cout << "Hello, Jenkins\n";
   cout << "I have successfully built and run this  \n";
-  cout << "Done with Lab 8 \n";
+  cout << "Webhook added successfully! \n";
   return 0;
 }
